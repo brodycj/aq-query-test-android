938548f6cc6e2385509b081cd7b122cb2d18c451
Christopher J. Brody <brodybits@litehelpers.net>
2015-11-03 10:06:03 +0100

    Initial test version

---
+++ src/io/liteglue/AQTest.java
@@ -0,0 +1,15 @@
+package io.liteglue;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AQTest extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
